6b362aa07de78f6cf60725d40096865e72f118ab
Sage <hello@sageelliott.com>
2020-03-09 11:26:20 -0700

    Import dog breed data for training

---
+++ 00-dog-breed-train.py
@@ -0,0 +1,138 @@
+#%%
+##########################
+# Imports 
+##########################
+
+# import system libraries
+import sys
+import os
+import glob
+
+# import Data Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt                        
+
+# %matplotlib inline
+
+# Set paths for custom modules
+sys.path.insert(0, './helpers')
+sys.path.insert(0, './models')
+
+# Data Loader functions
+from data_loader import dir_loader_stack
+from data_loader import image_plot
+
+# Model classes
+from resnet50 import Resnet50_pretrained
+
+# Model helpers
+from model_helpers import train
+from model_helpers import predict
+from model_helpers import plot_train_history
+
+# torch
+import torch.nn as nn
+import torch.optim as optim
+import torch
+
+
+#%%
+
+# option to show datasets & vis while running in py script
+verbose = True
+
+##########################
+# Data Paths 
+##########################
+
+
+# Dataset folder
+train_data_dir = '../datasets/dog_breeds/train'
+valid_data_dir = '../datasets/dog_breeds/valid'
+# test_data_dir = '../datasets/test_animals/'
+
+# %%
+##########################
+# Data parameters
+##########################
+
+img_size = 244
+batch_size = 32
+num_workers = 0
+
+
+# %% 
+##########################
+# Data Loaders
+##########################
+train_loader = dir_loader_stack(train_data_dir, img_size, batch_size,
+                                num_workers,shuffle=True)
+
+val_loader = dir_loader_stack(valid_data_dir, img_size, batch_size,
+                                num_workers,shuffle=False)
+
+# test_loader = csv_loader_stack(test_data_dir,test_df, 'FilePath', 'Label',
+#                         img_size,batch_size,num_workers,False)
+
+
+loaders = {
+    'train':train_loader,
+    'valid':val_loader,
+#     'test':test_loader,
+}
+
+# %%
+##########################
+# Verify Sample Data from data loaders
+##########################
+
+if verbose: 
+    # Train Data sample
+    image_plot(train_loader)
+
+# %%
+if verbose: 
+    # validation data sample
+    image_plot(val_loader)
+
+
+#%%
+##########################
+# Create model
+##########################
+
+# Number Classes to predict (dog breeds in image net)
+num_classes = 133
+
+# Compute device (cuda = GPU)
+device = 'cuda'
+
+# create model from model class
+res_model = Resnet50_pretrained(num_classes)
+
+#%%
+##########################
+# Train Model
+##########################
+
+# parameters
+n_epochs = 50
+learn_rate = 0.001
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(res_model.model.fc.parameters(), lr=learn_rate)
+
+device = 'cuda'
+save_path = 'trained_models/dog_breeds.pt'
+
+#%% 
+# Train 
+H = train(res_model.model, n_epochs, loaders, optimizer,
+                    criterion, device, save_path)
+
+
+#%%
+
+if verbose:
+    # Train Log
+    plot_train_history(H,n_epochs)
